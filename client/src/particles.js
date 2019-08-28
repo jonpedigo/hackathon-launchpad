8fd6fefc1dfa8f40ebd3409d16ee2af9d600117e
Jonathan Pedigo <jon.pedigo@feedly.com>
2019-08-28 16:19:50 -0700

    pixi particle emitter abstract

---
+++ client/src/particles.js
@@ -0,0 +1,73 @@
+import * as PIXI from 'pixi.js';
+const pixiParticles = require('pixi-particles');
+
+const flameEmitter = ({startPos, stage, particles = ['https://pixijs.io/pixi-particles-editor/assets/images/particle.png', 'https://pixijs.io/pixi-particles-editor/assets/images/Fire.png'], startEmitting = true}) => {
+  var emitter = new pixiParticles.Emitter(
+    stage,
+    particles.map(p => PIXI.Texture.from(p)),
+    {
+    	"alpha": {
+    		"start": 0.62,
+    		"end": 0
+    	},
+    	"scale": {
+    		"start": 0.25,
+    		"end": 0.75,
+    		"minimumScaleMultiplier": 1
+    	},
+    	"color": {
+    		"start": "#fff191",
+    		"end": "#ff622c"
+    	},
+    	"speed": {
+    		"start": 500,
+    		"end": 500,
+    		"minimumSpeedMultiplier": 1
+    	},
+    	"acceleration": {
+    		"x": 0,
+    		"y": 0
+    	},
+    	"maxSpeed": 0,
+    	"startRotation": {
+    		"min": 265,
+    		"max": 275
+    	},
+    	"noRotation": false,
+    	"rotationSpeed": {
+    		"min": 50,
+    		"max": 50
+    	},
+    	"lifetime": {
+    		"min": 0.1,
+    		"max": 0.75
+    	},
+    	"blendMode": "normal",
+    	"frequency": 0.001,
+    	"emitterLifetime": -1,
+    	"maxParticles": 1000,
+    	"pos": {
+    		"x": startPos.x,
+    		"y": startPos.y,
+    	},
+    	"addAtBack": false,
+    	"spawnType": "circle",
+    	"spawnCircle": {
+    		"x": 0,
+    		"y": 0,
+    		"r": 10
+    	}
+    }
+  );
+
+  if (startEmitting) {
+    emitter.emit = true
+  }
+
+  return emitter;
+}
+
+
+export {
+  flameEmitter,
+}
